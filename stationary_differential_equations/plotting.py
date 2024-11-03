6e50b0a9116ee18debb7fc0c6fbff2adbee2096e
ugniusalekna <ugnius.alekna@gmail.com>
2024-11-03 21:33:56 +0200

    Add spectrum visualizations of the 1D Laplacian with mixed BCs

---
+++ stationary_differential_equations/plotting.py
@@ -0,0 +1,205 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib.widgets import AxesWidget, RadioButtons
+from matplotlib import cbook
+import scienceplots
+
+plt.style.use('science')
+
+
+def show_plot(ax, x_data, y_data, 
+                   labels=None, 
+                   title=None, 
+                   x_label=None, 
+                   y_label=None, 
+                   secondary_y_data=None, 
+                   y2_label=None,
+                   legend_loc="best", 
+                   line_styles=None, 
+                   colors=None, 
+                   markers=None, 
+                   marker_sizes=None,
+                   line_widths=None,
+                   log_scale_x=False, 
+                   log_scale_y=False,
+                   grid=True,
+                   grid_style='--',
+                   grid_color='gray',
+                   grid_alpha=0.5,
+                   x_lim=None,
+                   y_lim=None,
+                   y2_lim=None,
+                   error_bars=None, 
+                   error_capsize=3,
+                   font_size=12,
+                   secondary_y_font_size=10,
+                   secondary_y_color='tab:red',
+                   set_int_xticks=False):
+    if not isinstance(y_data, list):
+        y_data = [y_data]
+    if not isinstance(x_data, list):
+        x_data = [x_data] * len(y_data)
+    
+    if labels is None:
+        labels = [f"Series {i+1}" for i in range(len(y_data))]
+    if line_styles is None:
+        line_styles = ['-'] * len(y_data)
+    if colors is None:
+        colors = [None] * len(y_data)
+    if markers is None:
+        markers = [None] * len(y_data)
+    if marker_sizes is None:
+        marker_sizes = [6] * len(y_data)
+    if line_widths is None:
+        line_widths = [1.5] * len(y_data)
+    if error_bars is None:
+        error_bars = [None] * len(y_data)
+
+    for i, (x, y) in enumerate(zip(x_data, y_data)):
+        if isinstance(colors[i], list) and len(colors[i]) == len(x):
+            # If colors[i] is a list of individual colors for each point
+            for xi, yi, color in zip(x, y, colors[i]):
+                ax.plot(xi, yi, 
+                        label=labels[i] if xi == x[0] else "",  # Only label the first point for legend
+                        linestyle=line_styles[i], 
+                        color=color, 
+                        marker=markers[i], 
+                        markersize=marker_sizes[i],
+                        linewidth=line_widths[i])
+        else:
+            # Use the same color for the entire series
+            ax.plot(x, y, 
+                    label=labels[i],
+                    linestyle=line_styles[i], 
+                    color=colors[i], 
+                    marker=markers[i], 
+                    markersize=marker_sizes[i],
+                    linewidth=line_widths[i])
+        
+        if error_bars[i] is not None:
+            ax.errorbar(x, y, yerr=error_bars[i], fmt='none', color=colors[i],
+                        capsize=error_capsize, elinewidth=line_widths[i])
+
+    if log_scale_x:
+        ax.set_xscale('log')
+    if log_scale_y:
+        ax.set_yscale('log')
+    
+    if grid:
+        ax.grid(True, linestyle=grid_style, color=grid_color, alpha=grid_alpha)
+
+    if x_label:
+        ax.set_xlabel(x_label, fontsize=font_size)
+    if y_label:
+        ax.set_ylabel(y_label, fontsize=font_size)
+    if title:
+        ax.set_title(title, fontsize=font_size + 2)
+
+    if x_lim:
+        ax.set_xlim(x_lim)
+    if y_lim:
+        ax.set_ylim(y_lim)
+    
+    if secondary_y_data is not None:
+        ax2 = ax.twinx()
+        for y2 in secondary_y_data:
+            ax2.plot(x_data[0], y2, color=secondary_y_color, linestyle='--')
+        if y2_label:
+            ax2.set_ylabel(y2_label, fontsize=secondary_y_font_size, color=secondary_y_color)
+        if y2_lim:
+            ax2.set_ylim(y2_lim)
+        ax2.tick_params(axis='y', labelcolor=secondary_y_color)
+    
+    ax.legend(loc=legend_loc, fontsize=font_size - 2)
+    
+    if set_int_xticks:
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+
+class CustomRadioButtons(AxesWidget):
+    def __init__(self, ax, labels, active=0, activecolor='blue', size=49,
+                 orientation="vertical", label_spacing=0.1, **kwargs):
+        """
+        Add custom radio buttons to an `~.axes.Axes`.
+
+        Parameters
+        ----------
+        ax : `~matplotlib.axes.Axes`
+            The axes to add the buttons to.
+        labels : list of str
+            The button labels.
+        active : int
+            The index of the initially selected button.
+        activecolor : color
+            The color of the selected button.
+        size : float
+            Size of the radio buttons.
+        orientation : str
+            The orientation of the buttons: 'vertical' (default), or 'horizontal'.
+        label_spacing : float
+            The spacing between the button markers and their labels.
+
+        Further parameters are passed on to `Legend`.
+        """
+        AxesWidget.__init__(self, ax)
+        self.activecolor = activecolor
+        axcolor = ax.get_facecolor()
+        self.value_selected = None
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_navigate(False)
+
+        circles = []
+        for i, label in enumerate(labels):
+            if i == active:
+                self.value_selected = label
+                facecolor = activecolor
+            else:
+                facecolor = axcolor
+
+            p = ax.scatter([], [], s=size, marker="o", edgecolor='black',
+                           facecolor=facecolor)
+            circles.append(p)
+
+        if orientation == "horizontal":
+            kwargs.update(ncol=len(labels), mode="expand")
+        kwargs.setdefault("frameon", False)    
+
+        self.box = ax.legend(circles, labels, loc="center", handletextpad=label_spacing, **kwargs)
+        self.labels = self.box.texts
+        self.circles = self.box.legend_handles
+
+        for c in self.circles:
+            c.set_picker(5)
+        
+        self._observers = cbook.CallbackRegistry()
+        
+        self.connect_event('pick_event', self._clicked)
+
+    def _clicked(self, event):
+        if (self.ignore(event) or event.mouseevent.button != 1 or
+                event.mouseevent.inaxes != self.ax):
+            return
+        if event.artist in self.circles:
+            self.set_active(self.circles.index(event.artist))
+
+    def set_active(self, index):
+        if 0 <= index < len(self.circles):
+            for i, p in enumerate(self.circles):
+                p.set_facecolor(self.activecolor if i == index else self.ax.get_facecolor())
+            self.value_selected = self.labels[index].get_text()
+            self._observers.process('clicked', self.value_selected)
+
+    def on_clicked(self, func):
+        """
+        Connect a function to the button click event.
+
+        Parameters
+        ----------
+        func : callable
+            A function with a single parameter that will be called
+            when a button is clicked. The parameter will be the text
+            label of the clicked button.
+        """
+        self._observers.connect('clicked', func)
---
+++ stationary_differential_equations/visualize_poisson_1d.py
@@ -0,0 +1,181 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+from matplotlib.widgets import Button, Slider
+
+from plotting import show_plot, CustomRadioButtons
+
+
+def create_matrix(N, alpha=None, beta=None, order=2):
+    coeffs = {
+        2: [-1.0, 2.0, -1.0],
+        4: [1/12, -4/3, 5/2, -4/3, 1/12],
+        6: [-1/90, 3/20, -3/2, 49/18, -3/2, 3/20, -1/90],
+        8: [1/560, -8/315, 1/5, -8/5, 205/72, -8/5, 1/5, -8/315, 1/560]
+    }
+
+    if order not in coeffs:
+        raise ValueError("No coeffs for this order of approximation.")
+
+    coeff = coeffs[order]
+    half = len(coeff) // 2
+
+    A = np.zeros((N, N))
+    
+    for i in range(-half, half + 1):
+        if i == 0:
+            continue
+        diag_values = np.full(N - abs(i), coeff[half + i])
+        A += np.diag(diag_values, k=i)
+    
+    A += np.diag(np.full(N, coeff[half]))
+
+    if alpha is not None and beta is not None:
+        h = 1 / (N + 1)
+        d = (alpha * h + beta)
+        if d != 0:
+            A[0, 0] -= (beta / d)
+            A[-1, -1] -= (beta / d)
+        else:
+            print("Division by zero in 'create_matrix'.")
+
+    return A
+
+
+def solve_eigen(A):
+    eigvals, eigvecs = la.eigh(A)
+
+    for i in range(eigvecs.shape[1]):
+        if np.sum(eigvecs[:, i]) < 0:
+            eigvecs[:, i] = -eigvecs[:, i]
+
+    return eigvals, eigvecs
+
+
+def plot_eigvals(eigvals, idx=None, ax=None):
+    n = len(eigvals)
+    colors = ['b'] * n
+    if idx is not None:
+        colors[idx] = 'r'
+
+    ax.clear()
+    show_plot(
+        ax=ax,
+        x_data=np.arange(1, n+1),
+        y_data=[eigvals],
+        labels=[r"Eigenvalues $\lambda_i$"],
+        title=rf"Eigenvalues of matrix $T_{{{n}}}$",
+        x_label=r"$i = 1,...,N$",
+        y_label=r"$\lambda_i$",
+        markers=['o'],
+        line_styles=['none'],
+        colors=[colors],
+        legend_loc="upper left",
+        set_int_xticks=True,
+    )
+    
+    if ax is not None:
+        plt.draw()
+
+
+def plot_eigvects(eigvecs, idx, ax=None):
+    y_lim = (-1.0, 1.0)
+    n = eigvecs.shape[0]
+    
+    ax.clear()
+    show_plot(
+        ax=ax,
+        x_data=np.arange(1, n+1),
+        y_data=eigvecs[:, idx],
+        labels=[rf"Eigenvector $v_{{{idx+1}}}$ corresponding to $\lambda_{{{idx+1}}}$"],
+        title=rf"Eigenvectors of matrix $T_{{{n}}}$",
+        x_label=r"$j = 1,...,N$",
+        y_label=rf"Coordinates $v^j_{{{idx+1}}}$",
+        markers=['o'],
+        line_styles=['-'],
+        colors=[plt.get_cmap('tab10')((idx%10))],
+        legend_loc="upper left",
+        y_lim=y_lim,
+        set_int_xticks=True,
+    )
+    
+    if ax is not None:
+        plt.draw()
+
+
+def plot_with_slider(N):
+    current_index = [0]
+    current_order = [2]
+
+    fig, (ax_vals, ax_vects) = plt.subplots(1, 2, figsize=(14, 7))
+    fig.subplots_adjust(hspace=0.4, bottom=0.2)
+
+    ax_slider_a = plt.axes([0.39, 0.08, 0.25, 0.02], facecolor='lightgray')
+    slider_alpha = Slider(ax_slider_a, r'Parameter $\alpha$', -5.0, 5.0, valinit=1.0, valstep=0.1, color='blue')
+    
+    ax_slider_b = plt.axes([0.39, 0.05, 0.25, 0.02], facecolor='lightgray')
+    slider_beta = Slider(ax_slider_b, r'Parameter $\beta$', -5.0, 5.0, valinit=0.0, valstep=0.1, color='blue')
+
+    ax_radio_order = plt.axes([0.12, 0.05, 0.16, 0.05], facecolor='lightgray')
+    radio_order =  CustomRadioButtons(ax_radio_order ,['2','4','6', '8'], active=0, orientation="horizontal", label_spacing=0.01)
+    order_text = fig.text(0.12, 0.11, rf'Order of approximation $\mathcal{{O}}(h^{{{current_order[0]}}})$', fontsize=12, weight='bold')
+
+    ax_next_button = plt.axes([0.81, 0.05, 0.1, 0.05])
+    ax_prev_button = plt.axes([0.7, 0.05, 0.1, 0.05])
+
+    next_button = Button(ax_next_button, 'Next')
+    prev_button = Button(ax_prev_button, 'Prev')
+    
+    def update(val):
+        alpha = slider_alpha.val
+        beta = slider_beta.val
+        order = current_order[0]
+        
+        A = create_matrix(N, alpha=alpha, beta=beta, order=order)
+        
+        global eigvals, eigvecs
+        eigvals, eigvecs = solve_eigen(A)
+        
+        global num_eigvecs
+        num_eigvecs = eigvecs.shape[1]
+
+        ax_vals.clear()
+        ax_vects.clear()
+        
+        plot_eigvals(eigvals, idx=current_index[0], ax=ax_vals)
+        plot_eigvects(eigvecs, idx=current_index[0], ax=ax_vects)
+
+    def update_order(label):
+        current_order[0] = int(label)
+        order_text.set_text(rf'Order of approximation $\mathcal{{O}}(h^{{{current_order[0]}}})$')
+        update(None)
+
+    def update_plot(forward=None):
+        if forward is None:
+            current_index[0] = 0
+        else:
+            step = 1 if forward else -1
+            current_index[0] = (current_index[0] + step) % num_eigvecs
+
+        plot_eigvects(eigvecs, idx=current_index[0], ax=ax_vects)
+        plot_eigvals(eigvals, idx=current_index[0], ax=ax_vals)
+
+    update(None) # init
+
+    next_button.on_clicked(lambda event: update_plot(forward=True))
+    prev_button.on_clicked(lambda event: update_plot(forward=False))
+
+    slider_alpha.on_changed(update)
+    slider_beta.on_changed(update)
+    radio_order.on_clicked(update_order)
+
+    plt.show()
+
+
+def main():
+    N = 101
+    plot_with_slider(N)
+
+
+if __name__ == '__main__':
+    main()
